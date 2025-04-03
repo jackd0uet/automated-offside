15feda6a4d482d84e80c551268115709b3e4a708
jqw2-douet <jack2.douet@live.uwe.ac.uk>
2025-04-03 12:55:18 +0100

    feat: database creation and integration

--- app/frontend/models.py
+++ app/frontend/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class OffsideDecision(models.Model):
+    id = models.AutoField(db_column='id', primary_key=True)
+    file_name = models.CharField(db_column="filename", null=False, blank=False, max_length=2048)
+    algorithm_offside = models.BooleanField(db_column='algorithm_offside', null=False, blank=False)
+    final_offside = models.BooleanField(db_column="final_offside", null=True, blank=False)
+    time_uploaded = models.DateField(db_column="time_uploaded", null=False, blank=False)
+    time_decided = models.DateField(db_column="time_decided", null=True, blank=False)
+
+    class Meta:
+        managed = True
+        db_table = "offside_decision"
+
+    def __str__(self) -> str:
+        return f"{self.id} | {self.file_name} |{self.algorithm_offside} | {self.final_offside} | {self.time_uploaded} | {self.time_decided}"
+    
+class ObjectDetection(models.Model):
+    id = models.AutoField(db_column='id', primary_key=True)
+    decision_id = models.ForeignKey(OffsideDecision, models.PROTECT, db_column='decision_id', blank=False, null=False)
+    player_detections = models.CharField(db_column="player_detections", max_length=2048)
+    keeper_detections = models.CharField(db_column="keeper_detections", max_length=2048)
+    ball_detections = models.CharField(db_column="ball_detections", max_length=2048)
+    ref_detections = models.CharField(db_column="ref_detections", max_length=2048)
+
+    class Meta:
+        managed = True
+        db_table = "object_detection"
+
+    def __str__(self) -> str:
+        return f"{self.id} | {self.decision_id} | {self.player_detections} | {self.keeper_detections} | {self.ball_detections} | {self.ref_detections}"
--- app/offside/settings.py
+++ app/offside/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "frontend",
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = "offside.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "offside_db",
+        "USER": os.environ.get("SQL_USER"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD"),
+        "HOST": "localhost",
+        "PORT": 3306,
     }
 }
 
